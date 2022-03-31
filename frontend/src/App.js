2b645d6a9e821fa794ee87b8432955dd54985ad9
Ira Sanchez <ira.d.sanchez@gmail.com>
2022-03-31 15:04:04 +0000

    Adds starting point
    - Includes description instead of CRA default
    - Adds Card file
    - Adds basic styling for the description
    - Does not include styling for card

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>
+        Introduction to React Workshop
+      </h1>
+      <h2>
+        Today we are going to build a simple application to practice two key
+        skills:
+      </h2>
+
+      <ul>
+        <li> Rendering things using React</li>
+        <li> Fetching data from an API</li>
+      </ul>
+
+      <h2>To do this we will</h2>
+      <ul>
+        <li >
+          Make a parent <span >and</span> child
+          component
+        </li>
+        <li >
+          Use the Fetch API <span >in</span> both
+          components
+        </li>
+        <li>In the parent component (App)</li>
+        <ul >
+          <li>
+            We will fetch a list of pokemon from this
+            url:
+            https://pokeapi.co/api/v2/pokemon
+
+          </li>
+        </ul>
+        <li>In the child component (Card)</li>
+        <ul>
+          <li >
+            We will fetch an individual pokemon's information using a different url for each pokemon
+          </li>
+        </ul>
+      </ul>
     </div>
   );
 }
